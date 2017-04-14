b778d4f2ea95bb86d6235c04e1e9446a86c473d9
Davide <Davide@DESKTOP-TFCBRG8>
2017-04-14 07:56:10 +0200

    Commit 201704140756

--- src/test/java/p1/WithAccessToken.java
+++ src/test/java/p1/WithAccessToken.java
@@ -11,24 +11,21 @@ public class WithAccessToken {
 	public static void main(String args[]) throws Exception {
 		// The factory instance is re-useable and thread safe.
 		TwitterFactory factory = new TwitterFactory();
-		AccessToken accessToken = loadAccessToken(Integer
-				.parseInt("0"/* args[0] */));
+		AccessToken accessToken = loadAccessToken(
+				Integer.parseInt("0"/* args[0] */));
 		Twitter twitter = factory.getInstance();
 		// twitter.setOAuthConsumerKey("zCr0zxWPdcDd8OKXnq65JWV3s",
 		// "iLoUAifY10Kbe22iLeV805mELn6v5biQuYWPHGa5QESo1qGUNl");
-		twitter.setOAuthConsumer("zCr0zxWPdcDd8OKXnq65JWV3s",
-				"iLoUAifY10Kbe22iLeV805mELn6v5biQuYWPHGa5QESo1qGUNl");
+		twitter.setOAuthConsumer("zCr0zxWPdcDd8OKXnq65JWV3s", "iLoUAifY10Kbe22iLeV805mELn6v5biQuYWPHGa5QESo1qGUNl");
 		twitter.setOAuthAccessToken(accessToken);
 
 		GregorianCalendar d1 = new GregorianCalendar();
 		System.out.println(d1.getTimeInMillis());
-		Status status = twitter
-				.updateStatus("paperino1" + d1.getTimeInMillis());
+		Status status = twitter.updateStatus("paperino1" + d1.getTimeInMillis());
 		// Status status = twitter.updateStatus(args[1]);
-		System.out.println("Successfully updated the status to ["
-				+ status.getText() + "].");
+		System.out.println("Successfully updated the status to [" + status.getText() + "].");
 
-		Query q1 = new Query("#12Aprile");
+		Query q1 = new Query("#chilhavisto");
 		q1.count(200);
 		q1.since("2017-01-01");
 		QueryResult qr1 = twitter.search(q1);
@@ -56,6 +53,44 @@ public class WithAccessToken {
 		for (long oneID : idlist1.getIDs()) {
 			System.out.println(twitter.lookupUsers(oneID).get(0).getName());
 		}
+
+		StatusListener listener = new StatusListener() {
+			public void onStatus(Status status) {
+				System.out.println(status.getUser().getName() + " : " + status.getText());
+				System.out.println("/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*");
+			}
+
+			public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+			}
+
+			public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+			}
+
+			public void onException(Exception ex) {
+				ex.printStackTrace();
+			}
+
+			public void onScrubGeo(long arg0, long arg1) {
+				// TODO Auto-generated method stub
+
+			}
+
+			public void onStallWarning(StallWarning arg0) {
+				// TODO Auto-generated method stub
+
+			}
+		};
+		TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+		twitterStream.setOAuthConsumer("zCr0zxWPdcDd8OKXnq65JWV3s",
+				"iLoUAifY10Kbe22iLeV805mELn6v5biQuYWPHGa5QESo1qGUNl");
+		twitterStream.setOAuthAccessToken(accessToken);
+		twitterStream.addListener(listener);
+		// sample() method internally creates a thread which manipulates
+		// TwitterStream and calls these adequate listener methods continuously.
+		twitterStream.sample();
+		twitterStream.filter("#chilhavisto");
+
+		Thread.sleep(60000);
 
 		System.exit(0);
 	}
